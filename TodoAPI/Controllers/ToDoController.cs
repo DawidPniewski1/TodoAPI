1976fd4ea561613c924fba240af73c7fa651ebb8
Dawid Pniewski <d.pniewski@gr8soft.pl>
2023-11-25 11:50:26 +0100

    init

---
+++ TodoAPI/Controllers/ToDoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoAPI.services;
+
+namespace TodoAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoController : ControllerBase
+    {
+        private readonly ToDoService _toDoService;
+
+        public ToDoController(ToDoService toDoService)
+        {
+            _toDoService = toDoService;
+        }
+
+
+        [HttpGet]
+        public  string GetTodos()
+        {
+            return _toDoService.GetTodos();
+        }
+    }
+}
---
+++ TodoAPI/services/ToDoService.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoAPI.services
+{
+    public class ToDoService
+    {
+
+        public string GetTodos()
+        {
+            return "test2";
+        }
+    }
+}
